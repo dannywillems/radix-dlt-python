5c64da4bc6f5ba690fc714e23a75143f9fa3915f
Danny Willems <contact@danny-willems.be>
2019-10-05 14:21:40 +0000

    Use six for abstract

--- radixdlt/network/client.py
+++ radixdlt/network/client.py
@@ -1,12 +1,14 @@
-from abc import ABC
+from abc import ABCMeta
+import six
 
 from radixdlt.universe.universe import Sunstone, Alphanet
 
 # https://docs.radixdlt.com/node-api/rest
 # https://docs.radixdlt.com/node-api/json-rpc
 
+
 # FIXME: https://docs.radixdlt.com/node-api/#rate-limits Rate limits!
-class AbstractClient(ABC):
+class AbstractClient(six.with_metaclass(ABCMeta)):
     UNIVERSE = None
 
     def __init__(self, client_ip):
